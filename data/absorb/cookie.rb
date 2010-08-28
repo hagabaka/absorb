41ff58d389804fe6d4cd57653e8609cfcea5db8a
Yaohan Chen <yaohan.chen@gmail.com>
2010-08-28 10:31:29 -0400

    use kcookiejar through DBus, not KIO::Integration
    With KIO::Integration::CookieJar setting/storing cookies sometimes fail, with
    the error message "QNetworkRequest::setHeader: QVariant of type
    QList<QNetworkCookie> cannot be used with header Cookie". This commit tries to
    fix the problem, but setting cookies still does not work because I cannot find
    the right way to pass a number as qlonglong to a DBus method.

--- data/absorb/cookie.rb
+++ data/absorb/cookie.rb
@@ -1,4 +1,38 @@
 require 'kio'
 
-Absorb::WebView.page.network_access_manager.cookie_jar = KIO::Integration::CookieJar.new
+# This cookie jar uses DBus to talk to the KDE cookie jar service. Based on
+# kio/khtml/accessmanager.cpp
+# http://api.kde.org/4.x-api/kdelibs-apidocs/kio/html/accessmanager_8cpp_source.html
+class Absorb::KDECookieJar < Qt::NetworkCookieJar
+  KCOOKIEJAR_DBUS = Qt::DBusInterface.new 'org.kde.kded', '/modules/kcookiejar',
+                                          'org.kde.KCookieServer'
+  WINDOW_ID = WebView.win_id
+
+  def cookiesForUrl(url)
+    kcookiejar('findDOMCookies', url.to_string).
+      split('; ').reject(&:empty?).map do |cookie|
+        Qt::NetworkCookie.new *cookie.split('=', 2).map(&Qt::ByteArray.method(:new))
+      end
+  end
+
+  def setCookiesFromUrl(cookies, url)
+    cookies.each do |cookie|
+      kcookiejar 'addCookies',
+                 url.to_string, 
+                 cookie.to_raw_form(Qt::NetworkCookie::Full), 
+                 Qt::Variant.from_value(WINDOW_ID, 'long')
+    end
+  end
+
+  def kcookiejar(method, *arguments)
+    reply = Qt::DBusReply.new KCOOKIEJAR_DBUS.call(method, *arguments)
+    unless reply.valid?
+      $stderr.puts reply.error.message
+    end
+    reply.value
+  end
+end
+
+page = Absorb::WebView.page
+page.network_access_manager.cookie_jar = Absorb::KDECookieJar.new(page)
 
